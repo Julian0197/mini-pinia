cf8b4f8f0b7bcbaf0c995adea05b2d9b25f79876
jandon.ma <jandon.ma@nio.com>
2023-07-21 17:18:24 +0800

    mini-pinia测试

---
+++ packages/mini-pinia/src/test/test.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <div>state.num:{{ useStore.num }}</div>
+    <div>getters.dnum:{{ useStore.dnum }}</div>
+    <button @click="addNum">增加</button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted, watchEffect } from "vue";
+import { useCounterStore } from "./counter";
+
+const useStore = useCounterStore();
+
+onMounted(() => {
+  setInterval(() => {
+    useStore.$reset();
+  }, 3000);
+
+  // setTimeout(() => {
+  //   useStore.$dispose();
+  // }, 9000);
+});
+
+watchEffect(() => {
+  console.log("watchEffect触发", useStore.num);
+});
+
+function addNum() {
+  useStore.addNum();
+  // useStore.$patch((state: any) => {
+  //   state.num++;
+  // });
+}
+</script>
